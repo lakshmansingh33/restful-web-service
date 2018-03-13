f8ac714559cb2655c6ab300ff68068e07629b4a5
Laxmansingh Muriyal <lakshman.muriyal@gmail.com>
2018-03-13 11:56:01 +0530

    updated the controller

--- src/main/java/com/practice/rest/webservice/restfulwebservice/helloWorld/HelloWorldController.java
+++ src/main/java/com/practice/rest/webservice/restfulwebservice/helloWorld/HelloWorldController.java
@@ -25,7 +25,7 @@ public class HelloWorldController {
 	
 	@GetMapping( path="/greetMessage")
 	public String greetMessage(){
-		return "Hi From greetMessage method with path /greetMessage";
+		return "Hi From greetMessage method";
 	}
 	
 	@GetMapping( path="/hello-world-bean")
